8c2c24ffd1791bcebf53b26f925e8d3b415bf8a4
dcleres <david.cleres@epfl.ch>
2017-10-09 09:51:03 +0200

    Serie03

---
+++ David Series/ex03/template/build_polynomial.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""implement a polynomial basis function."""
+
+import numpy as np
+
+
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    # ***************************************************
+    # polynomial basis function:
+    phi_mat = np.empty([len(x),degree+1])
+    for i in range(len(x)):
+        for j in range(degree+1):
+            phi_mat[i,j] = x[i]**j
+                
+    return phi_mat
+    # this function should return the matrix formed
+    # by applying the polynomial basis to the input data
+    # ***************************************************
