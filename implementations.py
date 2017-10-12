80d4f22619da18259c8275a8b1fc9c5058a830b8
dcleres <david.cleres@epfl.ch>
2017-10-12 15:52:33 +0200

    Most Recent Versions

--- implementations.py
+++ implementations.py
@@ -20,30 +20,25 @@ def compute_loss(y, tx, w, ltype="MSE"):
     if (ltype=="MAE"):
     # ***************************************************
     # Lost function by MAE
-        raise NotImplementedError
+        e = y - tx.dot(w)
+        losses = np.mean(np.abs(e))
     else:
     # ***************************************************
     # Lost function by MSE 
-        w = np.transpose(w); 
-        N = len(y)
         losses = (((y - np.dot(tx,w))**2).mean(axis = 0)) / 2 #Loss calculated using MSE
-    
-    
     return losses
 
-def gradient_descent(y, tx, initial_w, max_iters, gamma):
+def gradient_descent(y, tx, initial_w, max_iters, gamma, ltype="MSE"):
     """Gradient descent algorithm."""
-    
     ws = initial_w #initiate weights
     for n_iter in range(max_iters):
         
         gradient = compute_gradient (y, tx, ws) #compute the gradient for current weights
 
         # update w by gradient
-        
         ws = ws - gamma*gradient #updates the new weights
     
-    loss=compute_loss(y,tx,ws) #compute final error
+    loss=compute_loss(y,tx,ws,ltype) #compute final error
 
     return loss, ws
 
@@ -63,8 +58,8 @@ def compute_stoch_gradient(y, tx, w):
     gradient = -((1/N)*(np.dot(np.transpose(tx), y - np.dot(tx,w))))
     return gradient
     
-def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
-    """Stochastic gradient descent algorithm."""
+'''def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):  OLD  
+   """Stochastic gradient descent algorithm."""
     
     ws = initial_w
     g = 0
@@ -79,9 +74,24 @@ def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
             
     loss=compute_loss(y,tx,ws)
 
-    return loss, ws
+    return loss, ws'''
 
-def least_squares(y, tx, n_max):
+def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+    """Stochastic gradient descent algorithm."""
+    # ***************************************************
+    # implement stochastic gradient descent.
+    w = initial_w
+    g = 0
+    for n_iter in range(max_iters):
+        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size, num_batches=1):
+            grad, _ = compute_stoch_gradient(minibatch_y, minibatch_tx, w)
+            # update w by gradient
+            w = w - gamma*grad #computes the new w(t+1)
+            
+    loss = compute_loss(y, tx, w)
+    return losses, ws
+
+'''def least_squares(y, tx, n_max):  #ANTOINE
     """calculate the least squares solution."""
     xx=np.delete(tx,0,1) #save x vector
     for i in range(n_max-2):
@@ -89,9 +99,25 @@ def least_squares(y, tx, n_max):
     w_opt=np.linalg.solve(np.matmul(np.transpose(tx),tx),np.dot(np.transpose(tx),y)) #compute weights
     loss=compute_loss(y,tx,w_opt) #compute error
     
-    return loss, w_opt
+    return loss, w_opt'''
 
-def ridge_regression(y, tx, n_max, lambda_):
+def least_squares(y, tx): #DAVID
+    """calculate the least squares solution."""
+    a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+    return np.linalg.solve(a, b)
+
+def ridge_regression(y, tx, lambda_): #DAVID
+    """implement ridge regression."""
+    # ***************************************************
+    # ridge regression: 
+    tx_T = tx.transpose()
+    a = np.dot(tx_T,tx) + 2*len(y)*lambda_*np.identity(tx.shape[1])
+    b = np.dot(tx_T,y)
+    return np.linalg.solve(a,b) #this way is 1 µs faster
+    # ***************************************************
+    
+'''def ridge_regression(y, tx, n_max, lambda_): #ANTOINE
     """implement ridge regression."""
     lambda_=lambda_/(2*np.shape(y))
     xx=np.delete(tx,0,1) #save x vector
@@ -100,4 +126,37 @@ def ridge_regression(y, tx, n_max, lambda_):
     w_opt=np.linalg.solve(np.matmul(np.transpose(tx),tx)+lambda_*np.identity(np.shape(y)),np.dot(np.transpose(tx),y)) #compute weights
     loss=compute_loss(y,tx,w_opt) #compute error
     
-    return loss,w_opt
+    return loss,w_opt'''
+
+################### ADDITIONNAL REGRESSIONS ##############
+
+def compute_subgradient(y, tx, w):
+    """Compute the gradient."""
+    # ***************************************************
+    #compute gradient and loss
+    N = len(y)
+    
+    # MAE gradient: (demonstration is in notability ex6 - série 02)
+    gradient = -((1/N)*(np.dot(np.transpose(tx),np.sign(y - np.dot(tx,w)))))    
+    return gradient
+
+def compute_stoch_subgradient(y, tx, w):
+    """Compute a stochastic gradient from just few examples n and their corresponding y_n labels."""
+    # ***************************************************
+    # implement stochastic gradient computation.It's same as the gradient descent.
+    return compute_subgradient(y, tx, w)
+    
+def stochastic_subgradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+    """Stochastic gradient descent algorithm."""
+    # ***************************************************
+    # implement stochastic gradient descent.
+    w = initial_w
+    g = 0
+    for n_iter in range(max_iters):
+        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size, num_batches):
+            g = compute_stoch_subgradient(minibatch_y, minibatch_tx, w)        
+            #update w by gradient
+            w = w - gamma*g #computes the new w(t+1)
+    losses = compute_loss(y, tx, w)
+
+    return losses, w
